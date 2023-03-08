66495ca87dda70172f8e08bd031e3f468b1efd5d
yimeng436 <2441844062@qq.com>
2023-03-08 14:40:16 +0800

    init interface

---
+++ chatbot-api-interface/src/main/java/com/yhz/chatbot/CharBotInterface.java
@@ -0,0 +1,11 @@
+package com.yhz.chatbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CharBotInterface {
+    public static void main(String[] args) {
+        SpringApplication.run(CharBotInterface.class,args);
+    }
+}
